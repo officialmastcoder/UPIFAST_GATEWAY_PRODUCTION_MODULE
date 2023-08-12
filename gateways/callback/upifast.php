23cef52b63c8c9661ac265ebf4b735e880cc13ef
MAST CODER <96109325+officialmastcoder@users.noreply.github.com>
2023-08-12 18:08:06 +0530

    Update upifast.php

--- gateways/callback/upifast.php
+++ gateways/callback/upifast.php
@@ -35,7 +35,19 @@ if(isset($response['status']) &&  $response['status'] != NULL){
         $gateway_auth_key = $GATEWAY['upifast_module_auth_token'];
         $paramList = array('secret'=>urlencode($secret),'checksum'=>urlencode($checksum),'hash'=>urlencode($hash),'gateway_auth_key'=>urlencode($gateway_auth_key));
          $url  ="https://apizone.in/api/v6/verifyChecksum.php?param_list=".urlencode(json_encode($paramList));
-        $result = file_get_contents($url);
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	    CURLOPT_URL => $url,
+	    CURLOPT_RETURNTRANSFER => true,
+	    CURLOPT_ENCODING => "",
+	    CURLOPT_MAXREDIRS => 10,
+	    CURLOPT_TIMEOUT => 0,
+	    CURLOPT_FOLLOWLOCATION => true,
+	    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	    CURLOPT_CUSTOMREQUEST => "GET",
+	));
+	$result = curl_exec($curl);
+	curl_close($curl);
         $apiResdata = json_decode($result,true);
         
         if($apiResdata['status']==1){
